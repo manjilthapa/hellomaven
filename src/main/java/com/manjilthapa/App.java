456c838d1ab6b86b8dc9c8519a9f4e8bf29a2bc4
manjilthapa <manjilthapa@gmail.com>
2022-07-08 19:14:36 +0200

    initial commit

---
+++ src/main/java/com/manjilthapa/App.java
@@ -0,0 +1,13 @@
+package com.manjilthapa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
